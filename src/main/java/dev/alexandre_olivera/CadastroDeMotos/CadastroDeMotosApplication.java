5ac00bf7ae38e44d897c423aa0e1a0621119b707
Alexandre Oliveira <o.alexandrej14@gmail.com>
2025-03-14 21:34:09 -0300

    first commit

---
+++ src/main/java/dev/alexandre_olivera/CadastroDeMotos/CadastroDeMotosApplication.java
@@ -0,0 +1,13 @@
+package dev.alexandre_olivera.CadastroDeMotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeMotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeMotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alexandre_olivera/CadastroDeMotos/CadastroDeMotosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexandre_olivera.CadastroDeMotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeMotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
